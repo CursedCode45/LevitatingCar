1f41ec575ddd19f86b7b1dca99121cbdc99af626
leka45 <43352578+leka45@users.noreply.github.com>
2019-07-26 22:33:43 +0200

    Add files via upload

---
+++ Movement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float wMovement;
+    public float rotating;
+    public float rotationRate;
+    public float turnRotationSeekSpeed;
+    public Transform tr;
+
+    private float groundAngelVelocity;
+    private float rotationVelocity;
+
+    void Start()
+    {
+        Debug.Log("Works");
+    }
+
+    void FixedUpdate()
+    {
+        //This rotates the car at Y axis
+        Vector3 turnSmooth = Vector3.up * rotationRate * Input.GetAxis("Horizontal");
+        turnSmooth = turnSmooth * Time.deltaTime * rb.mass;
+        rb.AddTorque(turnSmooth);
+
+        //This makes a illusion of the rotation it only rotates the car but Z axis
+        Vector3 newRotation = transform.eulerAngles;
+        newRotation.z = Mathf.SmoothDampAngle(newRotation.z, Input.GetAxis("Horizontal") * -rotating, ref rotationVelocity, turnRotationSeekSpeed);
+        transform.eulerAngles = newRotation;
+
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(transform.forward * wMovement * 3);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(transform.forward * wMovement * -3);
+        }
+
+        if (Input.GetKey("space"))
+        {
+            transform.Rotate(new Vector3(-1, 0, 0) * 2f);
+        }
+    }
+}
---
+++ Thruster.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thruster : MonoBehaviour
+{
+    public float thrustersForce = 10;
+    public float maxDistance = 3f;
+    public Rigidbody rb;
+    public Transform[] thrusters;
+    Vector3 downForce;
+
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+
+        foreach (Transform thruster in thrusters)
+        {
+            Vector3 downForce;
+            float distancePercentage;
+
+            if (Physics.Raycast(thruster.position, thruster.up*-1, out hit, maxDistance))
+            {
+                distancePercentage = 1 - (hit.distance / maxDistance);
+                downForce = transform.up * thrustersForce * Time.deltaTime * rb.mass * distancePercentage;
+                rb.AddForceAtPosition(downForce, thruster.position);
+                Debug.DrawRay(thruster.position, thruster.up * -1 * maxDistance, Color.green);
+                rb.drag = 2f;
+            }
+            else
+            {
+                rb.drag = 0.5f;
+            }
+        }
+
+    }
+}
